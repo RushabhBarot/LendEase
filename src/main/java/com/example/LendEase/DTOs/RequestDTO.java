a0236bca16f0a854f73c899ddd6d71709bb264f0
Rushabh <rushabhbarot812@gmail.com>
2025-01-07 18:50:11 +0530

    added requestDTO

---
+++ src/main/java/com/example/LendEase/DTOs/RequestDTO.java
@@ -0,0 +1,30 @@
+package com.example.LendEase.DTOs;
+
+
+import com.example.LendEase.Entities.Enums.RequestStatus;
+import com.example.LendEase.Entities.Enums.RequestType;
+import com.example.LendEase.Entities.Pool;
+import com.example.LendEase.Entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestDTO {
+    private Long id;
+
+    private BigDecimal amount;
+    private BigDecimal interestRate;
+    private Integer durationInMonths;
+    private LocalDateTime createdAt;
+    private RequestType type;
+    private RequestStatus status;
+
+    private Pool pool;
+    private User user;
+}
